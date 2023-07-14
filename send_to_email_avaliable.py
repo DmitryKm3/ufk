88333292722692ad563b75231649dc52bf277fd2
DmitryKm3 <110657235+DmitryKm3@users.noreply.github.com>
2023-07-14 19:49:48 +0700

    14.07.23

---
+++ send_to_email_avaliable.py
@@ -0,0 +1,32 @@
+import smtplib as smtp
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+
+def send_email_about_avaliable(avaliable):
+    login = 'otchet064@gmail.com'
+    password = 'vbgzqbkjukpuppkv'
+
+    server = smtp.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(login, password)
+
+
+    subject = 'Сайт(ы) снова доступен(ы) после более часа недоступности.'
+
+    text = "" # здесь можно какай-то шаблон написать
+    for site in avaliable:
+        text += str(site) + "\n"
+
+    print(text)
+
+    message = MIMEText(text, 'plain', 'utf-8')
+    message['Subject'] = Header(subject, 'utf-8')
+
+    server.sendmail(login, 'lrr611@mail.ru', message.as_string())
+
+
+
+
+
